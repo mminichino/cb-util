a2a2e0b06812f78f0fbf7020d59314bb7f2dd838
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-01-18 09:24:21 -0600

    Version 2.2.0

--- tests/test_7.py
+++ tests/test_7.py
@@ -17,8 +17,6 @@ class TestRESTManager(object):
     @classmethod
     def setup_class(cls):
         cls.api_host = "cloudapi.cloud.couchbase.com"
-        logging.basicConfig()
-        logger.setLevel(logging.DEBUG)
 
     def test_1(self):
         rest = RESTManager(self.api_host)
@@ -36,12 +34,6 @@ class TestRESTManager(object):
         time_diff = end_time - start_time
         print(f"Item: {result} in {time_diff / 1000000}: OK")
 
-        start_time = time.perf_counter_ns()
-        result = rest.get_capella_kv(f"/v4/organizations/{org_id}/users", "email", "michael.minichino@couchbase.com").item(0).record()
-        end_time = time.perf_counter_ns()
-        time_diff = end_time - start_time
-        print(f"Item: {result.get('name')} in {time_diff / 1000000}: OK")
-
     def test_2(self):
         rest = RESTManager(self.api_host, key_file="pytest-project-api-key-token.txt")
         org_id = rest.get_capella('/v4/organizations').item(0).key('id')
@@ -57,9 +49,3 @@ class TestRESTManager(object):
         end_time = time.perf_counter_ns()
         time_diff = end_time - start_time
         print(f"Items: {result} in {time_diff / 1000000}: OK")
-
-        start_time = time.perf_counter_ns()
-        result = rest.get_capella_kv(f"/v4/organizations/{org_id}/users", "email", "michael.minichino@couchbase.com").item(0).record()
-        end_time = time.perf_counter_ns()
-        time_diff = end_time - start_time
-        print(f"Item: {result.get('name')} in {time_diff / 1000000}: OK")
